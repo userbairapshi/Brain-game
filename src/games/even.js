c04c6afaad8a1ce78b9aa62a4cd555b600afeb03
BAIRAPSHI <bairapshi@mail.ru>
2024-03-30 17:43:31 +0300

    modifited even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 import {
   mainPlay1,
-  mainPlay2,
   getRandNum,
 } from '../index.js';
 
@@ -9,12 +8,12 @@ const isEven = (num) => num % 2 === 0;
 
 const playEvenGame = () => {
   const name = mainPlay1();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   let correctAnswerCount = 0;
 
   while (correctAnswerCount < 3) {
     const number = getRandNum(1, 100);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
     console.log(`Question: ${number.toString()}`);
     const answer = readlineSync.question('Your answer: ');
     if ((isEven(number) && answer.toLowerCase() === 'yes') || (!isEven(number) && answer.toLowerCase() === 'no')) {
@@ -27,7 +26,7 @@ const playEvenGame = () => {
       return;
     }
   }
-  mainPlay2(name, correctAnswerCount);
+  mainPlay1(name, correctAnswerCount);
 };
 
 export default playEvenGame;
